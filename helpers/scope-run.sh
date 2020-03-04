cc0b2d265536d70207746e47abb646dfcfa25f7e
vasik <vaclav.struhar@mdh.se>
2020-03-04 11:14:37 +0100

    cscope for kernel

---
+++ helpers/scope-run.sh
@@ -0,0 +1,10 @@
+#LNX=/home/jru/linux-2.4.18
+find  $LNX                                                                \
+	-path "$LNX/arch/*" ! -path "$LNX/arch/i386*" -prune -o               \
+	-path "$LNX/include/asm-*" ! -path "$LNX/include/asm-i386*" -prune -o \
+	-path "$LNX/tmp*" -prune -o                                           \
+	-path "$LNX/Documentation*" -prune -o                                 \
+	-path "$LNX/scripts*" -prune -o                                       \
+	-path "$LNX/drivers*" -prune -o                                       \
+        -name "*.[chxsS]" -print > cscope.files
+#cscope -Rbkq
